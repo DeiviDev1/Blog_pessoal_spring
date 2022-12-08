52d2a6767047498e9fe2093d3eb201eb48e46e24
Deivison Rodrigues Rocha <deivison.rocha@mercadolivre.com>
2022-12-08 14:47:23 -0300

    realizando testes unitários

---
+++ src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
@@ -0,0 +1,104 @@
+package com.generation.blogpessoal.controller;
+
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+import com.generation.blogpessoal.service.UsuarioService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UsuarioControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeAll
+    void start(){
+
+        usuarioRepository.deleteAll();
+
+        usuarioService.cadastrarUsuario(new Usuario(0L,
+                "Root","root@root.com","rootroot", " "));
+    }
+
+
+    @Test
+    @DisplayName("Cadastrar um Usuário")
+    public void deveCriarUmUsuario(){
+
+        HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>
+                (new Usuario(0L, "Deivison Rocha","deivi@gmail.com","12345678","  "));
+
+        ResponseEntity<Usuario> corpoResposta = testRestTemplate
+                .exchange("/usuarios/cadastrar", HttpMethod.POST, corpoRequisicao, Usuario.class);
+
+        assertEquals(HttpStatus.CREATED, corpoResposta.getStatusCode());
+        assertEquals(corpoRequisicao.getBody().getNome(),corpoResposta.getBody().getNome());
+        assertEquals(corpoRequisicao.getBody().getUsuario(),corpoResposta.getBody().getUsuario());
+
+    }
+
+    @Test
+    @DisplayName("Listar todos os usuarios")
+    public void deveMostrarTodosUsuarios(){
+
+        usuarioService.cadastrarUsuario(new Usuario(0L,
+                "teste01","testetes01@gmail.com","12345678", " "));
+        usuarioService.cadastrarUsuario(new Usuario(0L,
+                "teste02","teste02@gmail.com","fghghfgh1142", " "));
+
+        ResponseEntity<String> resposta = testRestTemplate
+                .withBasicAuth("root@root.com","rootroot")
+                .exchange("/usuarios/all", HttpMethod.GET, null, String.class);
+
+        assertEquals(HttpStatus.OK,  resposta.getStatusCode());
+
+    }
+
+
+
+    @Test
+    @DisplayName("Atualizar um usuário")
+    public void deveAtualizarUmUsuario(){
+
+        Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(new Usuario(0L,
+                "Juliana Andrews", "juju@gmaail.com","juju12345678", "  "));
+
+        Usuario usuarioUpdate = new Usuario(usuarioCadastrado.get().getId(),
+        "Juliana Andrews Ramos", "juju@gmaail.com","juju12345678", "  ");
+        HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(usuarioUpdate);
+
+                ResponseEntity<Usuario>  corpoResposta= testRestTemplate
+                .withBasicAuth("root@root.com","rootroot")
+                .exchange("/usuarios/atualizar", HttpMethod.PUT, corpoRequisicao, Usuario.class);
+
+                assertEquals(HttpStatus.OK,corpoResposta.getStatusCode());
+                assertEquals(corpoRequisicao.getBody().getNome(), corpoResposta.getBody().getNome());
+                assertEquals(corpoRequisicao.getBody().getUsuario(),corpoResposta.getBody().getUsuario());
+
+    }
+
+
+
+
+}
---
+++ src/test/java/com/generation/blogpessoal/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.generation.blogpessoal.repository;
+
+
+import com.generation.blogpessoal.model.Usuario;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeAll
+    void start (){
+
+        usuarioRepository.deleteAll();
+        usuarioRepository.save(new Usuario( 0L,
+                "João da Silva", "joao@gmail.com", "123456789"," "  ));
+        usuarioRepository.save(new Usuario( 0L,
+                "Maria da Silva", "Maria@gmail.com", "8795358147"," "  ));
+        usuarioRepository.save(new Usuario( 0L,
+                "Adriana da Silva", "ij@gmail.com", "122645199"," "  ));
+        usuarioRepository.save(new Usuario( 0L,
+                "Jubilieu Silva ", "jubilei@gmail.com", "dsa287169117"," "  ));
+
+    }
+
+
+    @Test
+    @DisplayName("retorna 1 usuario")
+    public void deveRetornarUmUsuario(){
+
+        Optional<Usuario> usuario = usuarioRepository.findByUsuario("joao@gmail.com");
+        assertTrue(usuario.get().getUsuario().equals("joao@gmail.com"));
+    }
+
+    @Test
+    @DisplayName("retorna 3 usuarios")
+    public void deveRetornarTresUsuarios(){
+
+        List<Usuario> listaDeUsuarios = usuarioRepository.findAllByNomeContainingIgnoreCase("Silva");
+        assertEquals(3, listaDeUsuarios.size());
+        assertTrue(listaDeUsuarios.get(0).getNome().equals("João da Silva"));
+        assertTrue(listaDeUsuarios.get(1).getNome().equals("Maria da Silva"));
+        assertTrue(listaDeUsuarios.get(02).getNome().equals("Adriana da Silva"));
+    }
+
+
+    @AfterAll
+    public void end(){
+
+        usuarioRepository.deleteAll();
+    }
+
+}
